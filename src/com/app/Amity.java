323f3c3a9f2fe73200d7ab224c76b62d991d3c92
live-beta <wanjala.sam.s@gmail.com>
2017-08-29 17:05:20 +0300

    add(save_state): add logic for saving state data
    - Ensure that data that's active in the application is saved
    [Builds #Task-01]

--- src/com/app/Amity.java
+++ src/com/app/Amity.java
@@ -2,20 +2,89 @@ package com.app;
 
 import com.sun.deploy.security.MozillaMyKeyStore;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Amity {
 
-    private String people;
+   private List<Person>personList =new ArrayList<>();
+   private List<Room>roomList = new ArrayList<>();
+   private Connection connection = null;
+   private Statement statement = null;
+   private String sqlStatement ="";
 
-    public String loadPeopl(){
+   public boolean saveState(List<Person> personList,List<Room> roomList, String dbName){
 
-        return people;
-    }
-    public void loadState(){
+       /* Saving people data in the app */
 
-    }
-    public void saveState(){
+       if (personList.size() == 0 && roomList.size() == 0) {
 
-    }
+           System.out.println("Can not save empty data sets");
+           return false;
+
+       } else {
+
+           /* Save People data in the application*/
+
+           String url = "jdbc:sqlite:" + dbName + ".db";
+
+           try {
+               Class.forName("org.sqlite.JDBC");
+               connection = DriverManager.getConnection(url);
+
+               for (int i = 0; i <personList.size() ; i++) {
+
+                   String name = personList.get(i).toString();
+                   String category = personList.get(i).toString();
+                   String wants_accomodation = personList.get(i).toString();
+
+                   statement = connection.createStatement();
+                   sqlStatement = "INSERT INTO FELLOWS(NAME TEXT NOT NULL, CATEGORY TEXT NOT NULL,WANTS_ACCOMODATION TEXT NOT NULL)VALUES("+name+","+category+","+wants_accomodation+")";
+                   statement.execute(sqlStatement);
+
+               }
+
+
+           }catch (Exception e){
+               System.out.println(e.getMessage());
+               return false;
+           }
+
+           /* Save Room data in the application */
+
+           try {
+               Class.forName("org.sqlite.JDBC");
+               connection = DriverManager.getConnection(url);
+
+               for (int i = 0; i <roomList.size() ; i++) {
+
+                   String roomName = personList.get(i).toString();
+                   String roomType = personList.get(i).toString();
+                   String occupants = personList.get(i).toString();
+
+                   statement = connection.createStatement();
+                   sqlStatement = "INSERT INTO ROOMS(NAME TEXT NOT NULL, CATEGORY TEXT NOT NULL,OCCUPANTS TEXT NOT NULL)VALUES("+roomName+","+roomType+","+occupants+")";
+                   statement.execute(sqlStatement);
+
+               }
+
+           }catch (Exception e){
+               System.out.println(e.getMessage());
+               return false;
+           }
+           return true;
+       }
+
+   }
+
+   public boolean loadState(){
+
+       return false;
+   }
 
 
 }
