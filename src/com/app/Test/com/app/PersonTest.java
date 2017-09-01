fbe54de3984ac2bdbf45bdb4c3043acc84e004c6
live-beta <wanjala.sam.s@gmail.com>
2017-09-01 15:46:31 +0300

    refactore(person_test): refactor add person test
    - Ensure that the response given is read from an array list
    - Remove redundant commits
    
    [Builds #task-2]

--- src/com/app/Test/com/app/PersonTest.java
+++ src/com/app/Test/com/app/PersonTest.java
@@ -20,44 +20,34 @@ public class PersonTest {
         personData.setName("SAMMY WANJALA");
         personData.setCategory("FELLOW");
         personData.setRoomAllocation("VALHALLA");
-
         peopleList.add(personData);
 
     }
 
-    /* Test that fellow has been adde successfully to the system*/
     @Test
     public void test_that_a_person_has_been_added_successfully() {
 
-        genericMessage = person.addPerson("SAMMY WANJALA","FELLOW","Y","VALHALLA");
-        assertEquals(genericMessage, "Person has been added successfully");
+        peopleList = person.addPerson("SAMMY WANJALA", "FELLOW", "Y", "VALHALLA");
 
+        assertEquals(peopleList.get(0).getName(), "SAMMY WANJALA");
     }
 
     @Test
-    public void test_that_all_required_fields_are_given(){
+    public void test_that_fellow_has_been_reallocated_successfully() {
 
-        genericMessage = person.addPerson("SAMMY WANJALA","", "","");
-        assertEquals(genericMessage,"Ensure that you enter both name and category");
-
-    }
-    /* Test that fellow has been reallocated successfully*/
-    @Test
-    public void test_reallocate_person() {
         fail();
 
     }
-    /* Test that fellows can be printed out successfully */
+
     @Test
-    public void test_that_fellows_can_be_printed(){
+    public void test_that_fellows_can_be_printed() {
 
         genericMessage = person.printPeople(peopleList);
-        assertEquals(genericMessage,"SAMMY WANJALA");
+        assertEquals(genericMessage, "SAMMY WANJALA");
     }
 
-    /* Test that a fellows data is typed correctly in the appliaction*/
     @Test
     public void test_that_person_data_is_of_correct_type() {
- fail();
+        fail();
     }
 }
