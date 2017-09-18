e79b5980a583b0458b36b56f50d6626655e44a72
live-beta <wanjala.sam.s@gmail.com>
2017-09-18 19:26:06 +0300

    bg(amity test): refactor tests
    - Implements data collections
    - Adds save state logic
    - Add load State logic
    
    [Fixes #Task-03]

--- src/com/app/Test/com/app/AmityTest.java
+++ src/com/app/Test/com/app/AmityTest.java
@@ -1,39 +1,51 @@
 package com.app.Test.com.app;
 
-import com.app.Amity;
-import com.app.Person;
-import com.app.Room;
+import com.app.*;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.junit.Test;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class AmityTest {
 
-    Amity amity = new Amity();
-    private List<Person> personList = new ArrayList<>();
-    private List<Room> roomList = new ArrayList<>();
-    private boolean message;
+    Collection<List<Person>> personData= new ArrayList<>();
+    Collection<List<Room>> roomData= new ArrayList<>();
+    private dbModels models = new dbModels();
+    PersonOps personOps = new PersonOps("ELIUD KIMANI","STAFF","Y","PLATFORM");
+    RoomOps roomOps = new RoomOps("VALHALLA","OFFICE");
+    private List<Person> personList = personOps.addPerson();
+    private List<Room> roomList = roomOps.addRoom();
+    private boolean saveStateValue,loadStateValue;
 
-    /* Test that application can load state */
+
+    /**
+     * Test that the application can save state
+     */
 
     @Test
-    public void test_that_function_can_save_state(){
+    public void test_that_function_can_save_state() throws SQLException, ClassNotFoundException {
+        personData.add(personList);
+        roomData.add(roomList);
+        saveStateValue = models.saveState("testAmity",personData,roomData);
+        assertEquals(saveStateValue,true);
 
-//        message = amity.saveState(personList,roomList);
-//        assertEquals(message,true);
-
-        fail();
     }
 
     /* Test that application can load state */
     @Test
 
-    public void test_that_app_can_load_state(){
-        message = amity.loadState();
-        assertEquals(message,true);
+    public void test_that_app_can_load_state() throws SQLException,ClassNotFoundException{
+        HashMap<String,List> payLoad = models.loadState("testAmity");
+        personList = payLoad.get("People");
+        roomList = payLoad.get("Rooms");
+        String output = personList.get(0).getName() + " " + roomList.get(0).getRoomName();
+        assertEquals(output,"ELIUD KIMANI VALHALLA");
     }
 
 }
