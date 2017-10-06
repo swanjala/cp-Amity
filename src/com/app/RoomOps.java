b98f309f1b526bda01513ac4dfe4729a4c663a53
live-beta <wanjala.sam.s@gmail.com>
2017-10-06 21:13:47 +0300

    ft(shuffle box) : refactor suffle box
    - Add shuffle box functionality to roomOps
    
    [Builds #Task-Review]

--- src/com/app/RoomOps.java
+++ src/com/app/RoomOps.java
@@ -20,6 +20,7 @@ public class RoomOps extends Room {
 	public RoomOps(){
 
     }
+
     public RoomOps(String roomName, String roomCategory) {
 
             room.setRoomName(roomName);
@@ -44,6 +45,78 @@ public class RoomOps extends Room {
 
         roomList.add(room);
         return roomList;
+    }
+
+    /**
+     * shuffleBox : Generates a hash set of rooms  at random when a person
+     * is added to the application.
+     * It takes in a Collection of Lists of rooms, generates a random
+     * index , references a random room in the list and returns
+     * it
+     *
+     * @return
+     */
+
+    public static HashMap<String, String> shuffleBox(Collection<List<Room>> roomList) {
+
+        List<String> livingRoomNames = new ArrayList<>();
+        List<String> officeSpace = new ArrayList<>();
+        List<Room> result = new ArrayList<>();
+
+        HashMap<String, String> roomValues = new HashMap<>();
+
+        Room room = new Room();
+        Random randValue = new Random();
+
+        String livingRoomToAllocate;
+        String officeSpaceToAllocate;
+        int livingRoomIndex = 0;
+        int officeIndex = 0;
+
+
+        Iterator<List<Room>> iterator = roomList.iterator();
+
+        // Sort room collections by type
+
+        while (iterator.hasNext()) {
+
+            List<Room> roomIterator = iterator.next();
+
+
+            for (int index = 0; index < roomIterator.size(); index++) {
+
+                if (roomIterator.get(0).getRoomCategory().equals("LIVING")) {
+                    livingRoomNames.add(roomIterator.get(0).getRoomName());
+                } else if (roomIterator.get(0).getRoomCategory().equals("OFFICE")) {
+                    officeSpace.add(roomIterator.get(0).getRoomName());
+                }
+            }
+        }
+
+        // Select rooms using conditional logic
+
+        if (livingRoomNames.size()== 0){
+
+            livingRoomToAllocate ="Sample Room";
+
+        }else {
+            livingRoomIndex = randValue.nextInt(livingRoomNames.size());
+            livingRoomToAllocate = livingRoomNames.get(livingRoomIndex);
+        }
+
+        if (officeSpace.size() ==0){
+            officeSpaceToAllocate = "Sample Space";
+        } else {
+            officeIndex = randValue.nextInt(officeSpace.size());
+            officeSpaceToAllocate = officeSpace.get(officeIndex);
+        }
+
+        roomValues.put("Office", officeSpaceToAllocate);
+        roomValues.put("Living", livingRoomToAllocate);
+        roomValues.put("NoRespose", "No Room");
+
+        return roomValues;
+
     }
 
 }
